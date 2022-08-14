34c612d771c0d3072125d4ea111c8977873e5c4f
cmeireles1 <carinacmeireles@gmail.com>
2022-08-14 16:47:57 +0100

    slice.go

--- slice.go
+++ slice.go
@@ -7,26 +7,26 @@ import (
 )
 
 func main() {
-//Defining input as a string
+// Defining input as a string
 var input string
-//Making an empty slice of integers of size length 3
+// Making an empty slice of integers of size length 3
 sli := make([]int, 3)
 
-//For loop prompting user to enter an integer
+// For loop prompting user to enter an integer
 for {
 fmt.Println("Please enter an integer or enter x to exit:")
 fmt.Scan(&input)
-//If the user enters the character "x", the program quits
+// If the user enters the character "x", the program quits
 if input == "x" {
 	fmt.Println("Exiting program")
 	break
 }
-//Converting input to userInput with int type
+// Converting input to userInput with int type
 userInput, _ := strconv.Atoi(input)
-//Appending slice with userInput
+// Appending slice with userInput
 sli = append(sli, userInput)
-//Sorting integers in order and printing slice
+// Sorting integers in order and printing slice
 sort.Ints(sli[:])
 fmt.Println(sli)
- }
+}
 }
