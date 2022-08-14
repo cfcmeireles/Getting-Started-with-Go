e98c7a21b58c80caf8a3bf755f041f85b8fc2db6
cmeireles1 <carinacmeireles@gmail.com>
2022-08-14 16:46:30 +0100

    makejson.go

--- makejson.go
+++ makejson.go
@@ -6,24 +6,24 @@ import (
 )
 
 func main() {
-//Declaring name and address input as variables of string type
+// Declaring name and address input as variables of string type
 var nameInput string
 var addrInput string
-//Declaring idMap as a variable with the key and value of string type
+// Declaring idMap as a variable with the key and value of string type
 var idMap map[string]string
-//Prompting user to enter name and address and scanning nameInput and addrInput
+// Prompting user to enter name and address and scanning nameInput and addrInput
 fmt.Println("Please enter your name:")
 fmt.Scan(&nameInput)
 fmt.Println("Please enter your address:")
 fmt.Scan(&addrInput)
-//Creating idMap
+// Creating idMap
 idMap = make(map[string]string)
-//Defining "name" and "address" as keys in the map with nameInput and addrInput as values
+// Defining "name" and "address" as keys in the map with nameInput and addrInput as values
 idMap = map[string]string {
 "name": nameInput,
 "address": addrInput}
-//Creating a JSON object from the idMap
+// Creating a JSON object from the idMap
 jsonObj, _ := json.Marshal(idMap)
-//Printing the JSON object as a string
+// Printing the JSON object as a string
 fmt.Println(string(jsonObj))
 }
